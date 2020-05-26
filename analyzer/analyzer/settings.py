324ca4cf2015941cb72bc1ee927ea9b8abd709bb
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-26 14:46:43 +0530

    --- Auto-Git Commit ---

--- analyzer/analyzer/settings.py
+++ analyzer/analyzer/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +131,4 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+django_heroku.settings(locals())
